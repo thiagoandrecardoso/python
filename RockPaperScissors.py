49e702e3af65f45ba6024a0c834983f1c1575888
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-02-25 11:34:25 -0300

    Rename methods and attributes

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,28 +1,25 @@
 import random
 
 gestures = ["rock", "paper", "scissors"]
-
 n_round = 0
 n_rounds = 3
 rounds_to_win = n_rounds + 1 / 2
-
 cpu_score = 0
 player_score = 0
 
 
-def choiceCPUGesture():
+def choice_cpu_gesture():
     return random.choice(gestures)
 
 
-def choicePlayerGesture():
+def choice_player_gesture():
     choice = ""
     while choice != "rock" and choice != "paper" and choice != "scissors":
         choice = input("choose between 'rock', 'paper' or 'scissors'")
-
     return choice
 
 
-def whoWin():
+def who_win():
     if cpu_score == player_score:
         return 0
     elif cpu_score > player_score:
@@ -31,22 +28,22 @@ def whoWin():
         return 2
 
 
-def currentRound(cpu_score, player_score):
+def current_round(cpu_score, player_score):
     win = 0
-    choiceCPU = choiceCPUGesture()
-    choicePlayer = choicePlayerGesture()
-    if choiceCPU == "rock" and choicePlayer == "scissors" \
-            or choiceCPU == "paper" and choicePlayer == "rock" \
-            or choiceCPU == "scissors" and choicePlayer == "paper":
+    choice_cpu = choice_cpu_gesture()
+    choice_player = choice_player_gesture()
+    if choice_cpu == "rock" and choice_player == "scissors" \
+            or choice_cpu == "paper" and choice_player == "rock" \
+            or choice_cpu == "scissors" and choice_player == "paper":
         cpu_score += 1
         win = 1
-    if choicePlayer == "rock" and choiceCPU == "scissors" \
-            or choicePlayer == "paper" and choiceCPU == "rock" \
-            or choicePlayer == "scissors" and choiceCPU == "paper":
+    if choice_player == "rock" and choice_cpu == "scissors" \
+            or choice_player == "paper" and choice_cpu == "rock" \
+            or choice_player == "scissors" and choice_cpu == "paper":
         player_score += 1
         win = 2
-    print("CPU ", choiceCPU)
-    print("Player ", choicePlayer)
+    print("CPU ", choice_cpu)
+    print("Player ", choice_player)
     if win == 1:
         print("round winner CPU")
     if win == 2:
@@ -56,10 +53,10 @@ def currentRound(cpu_score, player_score):
 
 
 while cpu_score < rounds_to_win and player_score < rounds_to_win and n_round < n_rounds:
-    cpu_score, player_score = currentRound(cpu_score, player_score)
+    cpu_score, player_score = current_round(cpu_score, player_score)
     n_round += 1
 
-won = whoWin()
+won = who_win()
 if won == 0:
     print("Draw")
 elif won == 1:
